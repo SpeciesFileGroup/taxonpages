acdbea9b9acedef2658fe9ef1ba79bcc6c69e6ae
José Luis Pereira <pepelui@outlook.com>
2022-07-14 21:20:19 -0300

    Fix #8

--- src/modules/otus/components/TaxaInfo.vue
+++ src/modules/otus/components/TaxaInfo.vue
@@ -5,11 +5,6 @@
     </h2>
     <h1 class="text-xl dark:text-gray-100">
       <span v-html="taxonNameString" />
-      <span 
-        class="ml-2"
-        :class="statusStyle"
-        v-html="status"
-      />
     </h1>
     <h2 class="text-1xl text-gray-700 dark:text-gray-200">
       <CommonNames :otu-id="props.otuId" />
@@ -40,16 +35,4 @@ const taxonNameString = computed(() =>
     ? props.taxon.cached_html + ' ' + (props.taxon.cached_author_year || '')
     : props.taxon.original_combination
 )
-const status = computed(() => 
-  props.taxon.cached_is_valid
-    ? '&#10003;'
-    : '&#10005;'
-)
-
-const statusStyle = computed(() => 
-  props.taxon.cached_is_valid
-    ? 'text-green-600'
-    : 'text-red-600'
-)
-
 </script>
