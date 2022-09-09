ef54cdc6dcb5c4cba960d71cb633280ca1544124
José Luis Pereira <pepelui@outlook.com>
2022-09-09 15:04:04 -0300

    Move vars

--- src/components/Map/utils/geojsonOptions.js
+++ src/components/Map/utils/geojsonOptions.js
@@ -19,12 +19,29 @@ export default ({
   },
 
   style: (feature) => {
+    console.log(feature.properties?.base?.type)
     if (feature.properties?.base?.type === 'AssertedDistribution') {
       return { 
         color: 'rgb(var(--color-map-asserted))',
         weight: 1,
         dashArray: '3',
         dashOffset: '3',
+        fillOpacity: 0.5
+      }
+    }
+
+    if (feature.properties?.base?.type === 'TypeMaterial') {
+      return { 
+        color: 'rgb(var(--color-map-type-material))',
+        weight: 1,
+        fillOpacity: 0.5
+      }
+    }
+
+    if (feature.properties?.base?.type === 'CollectionObject') {
+      return { 
+        color: 'rgb(var(--color-map-collection-object))',
+        weight: 1,
         fillOpacity: 0.5
       }
     }
