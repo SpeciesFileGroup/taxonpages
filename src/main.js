baf36edc1f9ae686ade68eb078bcb63b4b9f0885
José Luis Pereira <pepelui@outlook.com>
2022-09-11 17:45:04 -0300

    Add setup module

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import SetupApp from './modules/setup/views/Index.vue'
 import router from '@/router/index.js'
 import globalComponents from '@/components/globalComponents'
 import.meta.globEager('@/assets/css/main.css')
 import.meta.globEager('../config/style/*.{scss,css}')
 
-const app = createApp(App)
+const isAPIConfigurationSet = __APP_ENV__.url && __APP_ENV__.project_token
+let app
 
-globalComponents.register(app)
-app.use(router)
+if (isAPIConfigurationSet) {
+  app = createApp(App)
+  app.use(router)
+  globalComponents.register(app)
+} else {
+  app = createApp(SetupApp)
+}
+
 app.mount('#app')
---
+++ src/modules/setup/views/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mx-auto">
+    <div class="container mx-auto flex flex-col items-center h-screen justify-center">
+      <h1 class="text-4xl font-medium">
+        Welcome to TaxonPages
+      </h1>
+      <p class="text-xl p-4">
+        We're almost there!
+        If you're seeing this, it's because you haven't configured API access yet.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
