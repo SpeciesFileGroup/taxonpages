dfb21d14b45e623ca008eb3b4a7a05b09a179de3
José Luis Pereira <pepelui@outlook.com>
2022-09-20 16:00:12 -0300

    Update main

--- src/main.js
+++ src/main.js
@@ -6,15 +6,22 @@ import globalComponents from '@/components/globalComponents'
 import.meta.globEager('@/assets/css/main.css')
 import.meta.globEager('../config/style/*.{scss,css}')
 
-const isAPIConfigurationSet = __APP_ENV__.url && __APP_ENV__.project_token
-let app
+function initTaxonPagesApp () {
+  const isAPIConfigurationSet = __APP_ENV__.url && __APP_ENV__.project_token
+  const app = createApp(
+    isAPIConfigurationSet
+      ? App
+      : SetupApp
+    )
 
-if (isAPIConfigurationSet) {
-  app = createApp(App)
-  app.use(router)
-  globalComponents.register(app)
-} else {
-  app = createApp(SetupApp)
+  if (isAPIConfigurationSet) {
+    app.use(router)
+    globalComponents.register(app)
+  }
+
+  return app
 }
+
+const app = initTaxonPagesApp()
 
 app.mount('#app')
