f67c2ec8cd57e53768d84d4364fa17e6185f9a7b
José Luis Pereira <pepelui@outlook.com>
2022-11-01 15:51:44 -0300

    Add invalid mark

--- src/modules/otus/components/TaxaInfo.vue
+++ src/modules/otus/components/TaxaInfo.vue
@@ -5,6 +5,11 @@
     </h2>
     <h1 class="text-xl dark:text-gray-100">
       <span v-html="taxonNameString" />
+      <span 
+        class="ml-2"
+        :class="statusStyle"
+        v-html="status"
+      />
     </h1>
     <h2 class="text-1xl">
       <CommonNames :otu-id="props.otuId" />
@@ -27,6 +32,17 @@ const props = defineProps({
     default: undefined
   }
 })
+
+const status = computed(() => 
+  props.taxon.cached_is_valid
+    ? ''
+    : '&#10005;'
+)
+const statusStyle = computed(() => 
+  props.taxon.cached_is_valid
+    ? ''
+    : 'text-red-600'
+)
 
 const isValid = computed(() => props.taxon.id === props.taxon.cached_valid_taxon_name_id)
 
