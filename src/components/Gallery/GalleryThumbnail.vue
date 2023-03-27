3ece11198c6d8f773015ef97200b5a681966d218
jlpereira <pepelui@outlook.com>
2023-03-27 20:17:07 -0300

    Update thumbnail

--- src/components/Gallery/GalleryThumbnail.vue
+++ src/components/Gallery/GalleryThumbnail.vue
@@ -1,18 +1,17 @@
 <template>
-  <div 
+  <div
     class="bg-base-background flex flex-col justify-center cursor-pointer w-24 max-h-20 h-20 border-base-muted rounded-md border"
     :title="title"
   >
-    <img 
-      class="max-h-20 max-w-24 h-20 w-24 object-cover rounded"
+    <img
+      class="max-h-20 max-w-24 h-20 w-24 object-contain rounded"
       :alt="title"
       :src="image.thumb"
-    >
+    />
   </div>
 </template>
 
 <script setup>
-
 defineProps({
   image: {
     type: Object,
