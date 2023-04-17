b6abdbcdfd47b7ba00a7928dc924c8f1a73ea01f
jlpereira <pepelui@outlook.com>
2023-04-17 11:44:47 -0300

    Remove default homepage

--- src/modules/home/views/index.vue
+++ src/modules/home/views/index.vue
@@ -1,24 +1,11 @@
 <template>
   <div class="container mx-auto text-center mt-36 flex flex-col justify-center">
-    <h1 class="text-4xl">
-      Welcome to TaxonPages
-    </h1>
+    <h1 class="text-4xl">Welcome to TaxonPages</h1>
     <p class="m-4">
-      It seems you don't have a index or home file in your /pages folder, create one to replace this page.
+      It seems you don't have a index or home file in your /pages folder, create
+      one to replace this page.
     </p>
-    <div class="flex justify-center">
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      />
-    </div>
   </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
