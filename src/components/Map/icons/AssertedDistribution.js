2c17242e09ce99f82d128f24abadb6e377788022
jlpereira <pepelui@outlook.com>
2023-04-18 15:21:31 -0300

    Refactor geojson options

--- src/components/Map/icons/AssertedDistribution.js
+++ src/components/Map/icons/AssertedDistribution.js
@@ -1,7 +1,6 @@
 import L from 'leaflet'
-
-export const AssertedDistribution = L.divIcon({
+export const AssertedDistribution = {
   className: 'bg-map-asserted bg-opacity-50 rounded-full',
   iconSize: [8, 8],
   iconAnchor: [4, 4]
-})
+}
