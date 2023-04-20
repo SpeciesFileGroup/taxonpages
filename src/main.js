9bb9bbc648a7efa2e8b5f44596929f33a576b003
jlpereira <pepelui@outlook.com>
2023-04-20 15:55:42 -0300

    Clean up

--- src/main.js
+++ src/main.js
@@ -1,28 +1,3 @@
-/* import { createApp } from 'vue'
-import App from './App.vue'
-import SetupApp from './modules/setup/views/Index.vue'
-import router from '@/router/index.js'
-import globalComponents from '@/components/globalComponents'
-import.meta.globEager('@/assets/css/main.css')
-import.meta.globEager('../config/style/*.{scss,css}')
-
-function initTaxonPagesApp() {
-  const isAPIConfigurationSet = __APP_ENV__.url && __APP_ENV__.project_token
-  const app = createApp(isAPIConfigurationSet ? App : SetupApp)
-
-  if (isAPIConfigurationSet) {
-    app.use(router)
-    globalComponents.register(app)
-  }
-
-  return app
-}
-
-const app = initTaxonPagesApp()
-
-app.mount('#app')
- */
-
 import.meta.glob('@/assets/css/main.css', { eager: true })
 import.meta.glob('../config/style/*.{scss,css}', { eager: true })
 
