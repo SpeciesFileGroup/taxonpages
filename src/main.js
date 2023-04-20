39f50a8be3ee9287ab07fc6bcc17d4bec8a4c001
jlpereira <pepelui@outlook.com>
2023-04-20 13:22:56 -0300

    Add setup view

--- src/main.js
+++ src/main.js
@@ -27,12 +27,14 @@ import.meta.glob('@/assets/css/main.css', { eager: true })
 import.meta.glob('../config/style/*.{scss,css}', { eager: true })
 
 import App from './App.vue'
+import SetupApp from './modules/setup/views/Index.vue'
 import { createPinia } from 'pinia'
 import { createSSRApp } from 'vue'
 import { createRouter } from './router'
 
 export function createApp() {
-  const app = createSSRApp(App)
+  const isAPIConfigurationSet = __APP_ENV__.url && __APP_ENV__.project_token
+  const app = createSSRApp(isAPIConfigurationSet ? App : SetupApp)
   const router = createRouter()
   const store = createPinia()
 
