757976f6a3e3941bedcb2e30c916d37c5d3e4617
jlpereira <pepelui@outlook.com>
2023-04-21 17:36:53 -0300

    Add unhead package. Add meta for spa and ssr

---
+++ src/utils/getMeta.js
@@ -0,0 +1,13 @@
+export function getMetaFromConfig(configObj) {
+  const metaObj = Object.entries(configObj).filter(([key, value]) =>
+    key.startsWith('meta_')
+  )
+
+  return metaObj.map(([key, value]) => {
+    const metaKey = key.slice(5)
+
+    return {
+      [metaKey]: value
+    }
+  })
+}
