7bf512112d2da9675cf696a438afd48443a3c32b
jlpereira <pepelui@outlook.com>
2023-06-11 13:59:23 -0300

    Update stats panel

--- src/components/Button/ButtonExpand.global.vue
+++ src/components/Button/ButtonExpand.global.vue
@@ -1,16 +1,12 @@
 <template>
   <button
     type="button"
-    class="
-      button-expand
-      bg-base-foreground
-      text-secondary-color
-      w-5
-      h-5"
+    :title="isExpanded ? 'Close' : 'Expand'"
+    class="button-expand bg-base-foreground text-secondary-color w-5 h-5"
     @click="isExpanded = !isExpanded"
   >
     <IconMinusCircle
-      v-if="isExpanded" 
+      v-if="isExpanded"
       class="w-5 h-5"
     />
     <IconPlusCircle
@@ -21,7 +17,6 @@
 </template>
 
 <script setup>
-
 import { computed } from 'vue'
 
 const props = defineProps({
@@ -34,13 +29,12 @@ const props = defineProps({
 const emit = defineEmits(['update:modelValue'])
 
 const isExpanded = computed({
-  get () {
+  get() {
     return props.modelValue
   },
 
-  set (value) {
+  set(value) {
     emit('update:modelValue', value)
   }
 })
-
 </script>
