be2ca015a5100376de9953105643c9ec32a84eb9
Davide Dal Pos <daveliga@gmail.com>
2023-11-26 17:17:49 -0500

    s

--- pages/home2.vue
+++ pages/home2.vue
@@ -1,14 +1,6 @@
 <template>
     <div class="h-full">
-      <SectionLandscape />
       <SectionData />
-      <hr class="border-base-muted" />
-      <SectionDescription />
-      <SectionAuthor class="shadow-sm" />
-      <hr class="border-base-muted" />
-  
-      <SectionAnnouncements />
-      <SectionTerms class="drop-shadow" />
     </div>
   </template>
   <script setup>
