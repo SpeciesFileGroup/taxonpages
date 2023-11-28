041fe295bce92ede67b8f35d3707355484e6738a
Davide Dal Pos <daveliga@gmail.com>
2023-11-28 17:32:32 -0500

    s

--- pages/components/Section/SectionLimitations.vue
+++ pages/components/Section/SectionLimitations.vue
@@ -1,7 +1,7 @@
 <template>
     <section>
       <div class="mx-auto py-6 prose !container dark:prose-invert">
-        <h2 class="px-4 sm:px-0">Limitations</h2>
+        <h2 class="px-4 sm:px-0">Highlights</h2>
         <div class="flex gap-4 flex-col">
           <VCard>
             <VCardContent>
@@ -15,7 +15,18 @@
               </p>
             </VCardContent>
           </VCard>
-  
+          
+          <VCard>
+          <h3 class="mt-2 mb-0">Taxa Completed</h3>
+              <span class="text-base-soft">2023-10-09</span>
+              <p>
+                Not all the subfamilies are completed, menaing that some names are missing.
+                Therefore, before citing the contents, make sure to take a look at the
+                documen XXX.
+              </p>
+            </VCardContent>
+          </VCard>  
+
           <VCard>
             <VCardContent>
               <h3 class="mt-2 mb-0">OSF Powered by TaxonWorks</h3>
@@ -26,11 +37,11 @@
                 filtering and reporting functionalities. Please contact us
                 (<address-maker
                   class="font-medium"
-                  :items="['cigliano', 'fcnym', 'unlp', 'edu', 'ar']"
+                  :items="['davide.dalpos', 'ucf', 'edu']"
                 />;
                 <address-maker
                   class="font-medium"
-                  :items="['braun', 'fcnym', 'unlp', 'edu', 'ar']"
+                  :items="['filippod', 'unisiena', 'edu']"
                 />) if you need extended access to the data. Over time we expect
                 this site to gradually mirror these extended functionalities of
                 TaxonWorks.
