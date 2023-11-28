c69d754d7072ae6863cf53ff4602554930b42f2c
Davide Dal Pos <daveliga@gmail.com>
2023-11-28 15:20:06 -0500

    s

--- pages/components/ImageCarrousel.vue
+++ pages/components/ImageCarrousel.vue
@@ -35,78 +35,12 @@ const props = defineProps({
 })
 
 const images = [
-  {
-    label: 'Acanthacris ruficornis citrina',
-    copyright: 'Michel Lecoq',
-    src: acanthacrisRuficornisCitrina,
-    otuId: 819322
-  },
-  {
-    label: 'Typophyllum inflatum',
-    copyright: 'Holger Braun',
-    src: typophyllumInflatum,
-    otuId: 850435
-  },
-  {
-    label: 'Xenicola dohrni',
-    copyright: 'Holger Braun',
-    src: xenicolaDohrni,
-    otuId: 848543
-  },
+
   {
     label: 'Lebinthus luae',
     copyright: 'Ming Kai Tan',
     src: lebinthusLuae,
     otuId: 831633
-  },
-  {
-    label: 'Liladownsia fraile',
-    copyright: 'Ricardo Mariño-Pérez',
-    src: liladownsiaFraile,
-    otuId: 816243
-  },
-  {
-    label: 'Aerotegmina vociferator',
-    copyright: 'Claudia Hemp',
-    src: aerotegminaVociferator,
-    otuId: 850176
-  },
-  {
-    label: 'Chlorobalius leucoviridis',
-    copyright: 'David Rentz',
-    src: chlorobaliusLeucoviridis,
-    otuId: 850075
-  },
-
-  {
-    label: 'Chromacris speciosa',
-    src: chromacrisSpeciosa,
-    copyright: 'Cigliano, M.M',
-    otuId: 823304
-  },
-  {
-    label: 'Chibchacris fernandezi',
-    src: chibchacrisFernandezi,
-    copyright: 'Cigliano, M.M',
-    otuId: 816353
-  },
-  {
-    label: 'Coryacris angustipennis',
-    src: coryacrisAngustipennis,
-    copyright: 'Cigliano, M.M',
-    otuId: 823314
-  },
-  {
-    label: 'Stenopola puncticeps',
-    copyright: 'Cigliano, M.M',
-    src: stenopolaPuncticeps,
-    otuId: 820284
-  },
-  {
-    label: 'Zoniopoda tarsata',
-    copyright: 'Cigliano, M.M',
-    src: zoniopodaTarsata,
-    otuId: 823378
   }
 ]
 
