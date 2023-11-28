c2640965aea8b2ff20f241e74d40a028609826ae
Davide Dal Pos <daveliga@gmail.com>
2023-11-28 15:53:40 -0500

    s

---
+++ pages/news2.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="h-full">
+      <SectionAnnouncements />
+    </div>
+  </template>
+  <script setup>
+  import SectionAnnouncements from './components/Section/SectionAnnouncements.vue'
+  </script>
