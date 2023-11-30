74c82f7c5f705b6b06559169ed0685a25ec5a20c
Davide Dal Pos <daveliga@gmail.com>
2023-11-30 16:09:30 -0500

    s

---
+++ pages/components/Section/DataTable.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps( props: {
+    items: {
+        type: Array,
+        required: true
+    }
+});
+
+</script>
+
+<template>
+  <table>
+    <tr v-for="item in items" :key="item.id">
+        <td>{{ item.id }}</td>
+
+    </tr>
+
+
+  </table>  
+</template>    
--- pages/subfamilies.vue
+++ pages/subfamilies.vue
@@ -1,11 +1,23 @@
 <script setup>
+import DataTable from './components/Section/DataTable.vue';
+
+const item = [
+    {
+        id: 2
+    },
+    {
+        id: 3
+    }
+
+];
+
 
 
 </script>
 
 <template>
   <div class="p-8 bg-gray-100 min-h-screen">
-    
+    <DataTable :items="items" /S>
   </div>  
 
 </template>    
