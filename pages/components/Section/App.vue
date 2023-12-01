db9e039bec5eea255ce151a12b967b82b5e78f67
Davide Dal Pos <daveliga@gmail.com>
2023-12-01 11:50:53 -0500

    s

--- pages/components/Section/App.vue
+++ pages/components/Section/App.vue
@@ -1,10 +1,21 @@
 <script setup>
+import DataTable from './Data/DataTable.vue';
+
+
+const items = [
+    {
+        id: 2
+    },
+    {
+        id: 3
+    }
+];
 
 </script>
 
 
 <template>
     <div class="p-8 bg-gray-100 min-h-screen">
-        
+        <DataTable :items="items" />
     </div>
 </template>
---
+++ pages/components/Section/Data/DataTable.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps(props: {
+    items: {
+        type: Array,
+        required: true
+    }
+});
+</script>
+    
+<template>
+   <table>
+    <tr v-for="item in items" :key="item.id">
+        <td>{{ item.id }}</td>
+    </tr>
+   </table>
+</template>
