bb7ee16c7b5f207bdd44eff004bc76171bc9029e
Davide Dal Pos <daveliga@gmail.com>
2023-12-03 12:34:38 -0500

    search

--- pages/components/Section/Data/SearchForm.vue
+++ pages/components/Section/Data/SearchForm.vue
@@ -3,5 +3,10 @@
 </script>
 
 <template>
-    
+    <form>
+        <label>Search for subfamily...</label>
+        <div>
+            <input type="text" placeholder="Search for subfamily...">
+        </div>
+    </form>
 </template>
