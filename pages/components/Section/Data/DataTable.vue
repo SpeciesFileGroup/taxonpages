c7d222d42adee80c03b18f54f7306aba76c65d5a
Davide Dal Pos <daveliga@gmail.com>
2023-12-03 18:56:51 -0500

    search

--- pages/components/Section/Data/DataTable.vue
+++ pages/components/Section/Data/DataTable.vue
@@ -45,7 +45,7 @@ const props = defineProps({
              <td class="px-4 py-3">{{ item.citations }}</td>
              <td class="px-4 py-3">{{ item.biology }}</td>
              <td class="px-4 py-3 flex items-center justify-end">
-                <router-link v-bind:to="link" class="text-indigo-500 hover:underline" target="_blank">{{ item.anchor}}</router-link>
+                <router-link v-bind:to="item.link" class="text-indigo-500 hover:underline" target="_blank">{{ item.anchor}}</router-link>
             </td>
             </tr> 
         </tbody>
