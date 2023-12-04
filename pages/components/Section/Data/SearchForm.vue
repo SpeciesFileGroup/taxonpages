9b6dbb6741d89127c1765f6fe037a1e97e18a2dc
Davide Dal Pos <daveliga@gmail.com>
2023-12-04 17:11:12 -0500

    search

--- pages/components/Section/Data/SearchForm.vue
+++ pages/components/Section/Data/SearchForm.vue
@@ -14,9 +14,9 @@ const search = (e) => {
 <template>
     <form class="py-3 px-4 flex items-center">
         <label class="sr-only">Search</label>
-        <div class="w-full">
+        <div class=" relative w-full">
             <input type="text" @input="search" placeholder="Search for subfamily..." 
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded">
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded height-5">
         </div>
     </form>
 </template>
