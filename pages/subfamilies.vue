513f522fdf89d2d89067845bc06938f62671ee71
Davide Dal Pos <daveliga@gmail.com>
2023-12-10 16:21:55 -0500

    search

--- pages/subfamilies.vue
+++ pages/subfamilies.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="h-full">
-      <App />
+      <AppNew />
     </div>
   </template>
   <script setup>
   import App from './components/Section/App.vue';
+import AppNew from './components/Section/AppNew.vue';
   </script>
