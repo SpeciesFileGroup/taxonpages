9d6f04ee3ae0fc9159accabf675dcd59ccdbd7ff
Davide Dal Pos <daveliga@gmail.com>
2024-01-17 11:09:59 -0500

    Table_paragraph

--- pages/components/subfamilies/DataTable.vue
+++ pages/components/subfamilies/DataTable.vue
@@ -22,36 +22,34 @@ const list = computed(() =>
 )
 </script>
 
-<template>
-<section class="bg-base-foreground">
-  <div class="container mx-auto p-4 sm:pt-10 sm:pb-2 relative box-border">
-    <div class="prose !container dark:prose-invert">
-      <div>
-        <p>
-          The <b>World Ichneumonidae Database (WID)</b> is a comprehensive and 
-          collaborative database for world fauna of Ichneumonidae managed in 
-          <a href="https://taxonworks.org"> TaxonWorks</a>. In this portal 
-          you can find information about distribution, nomenclature, 
-          and biology of the world Darwin wasps.
-        </p>
-        <p>
-          The core purpose of the WID is 
-          the research on Ichneumonidae by providing a centralized resource for images, 
-          reference, and taxonomy/nomenclature. Moreover, being updateble and open access,
-          the portal is designed to include all the relevant information updated 
-          constantly.
-        </p>
-        <p>
-          WID is open to contributions from anyone with specimen records and
-          images.
-        </p>
+<template>  
+  <section class="bg-base-foreground">
+    <div class="container mx-auto p-4 sm:pt-10 sm:pb-2 relative box-border">
+      <div class="prose !container dark:prose-invert">
+        <div>
+          <p>
+            The <b>World Ichneumonidae Database (WID)</b> is a comprehensive and 
+            collaborative database for world fauna of Ichneumonidae managed in 
+            <a href="https://taxonworks.org"> TaxonWorks</a>. In this portal 
+            you can find information about distribution, nomenclature, 
+            and biology of the world Darwin wasps.
+          </p>
+          <p>
+            The core purpose of the WID is 
+            the research on Ichneumonidae by providing a centralized resource for images, 
+            reference, and taxonomy/nomenclature. Moreover, being updateble and open access,
+            the portal is designed to include all the relevant information updated 
+            constantly.
+          </p>
+          <p>
+            WID is open to contributions from anyone with specimen records and
+            images.
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-</section>
-</template>
+  </section>
 
-<template>  
   <div class="bg-base-foreground rounded-lg">
     <div class="flex items-center justify-between">
       <SearchForm v-model="filters.subfamily" />
