e3de06fdc3e79818280229246fac81dc109573cd
Davide Dal Pos <daveliga@gmail.com>
2024-01-17 11:39:11 -0500

    Description

--- pages/components/subfamilies/subfamiliesData.js
+++ pages/components/subfamilies/subfamiliesData.js
@@ -6,7 +6,7 @@ export default [
     citations: 'OK',
     biology: 'Incomplete',
     anchor: 'Link',
-    link: '/Description/Acaenitinae'
+    link: '/Acaenitinae'
   },
   {
     id: 'Adelognathinae',
