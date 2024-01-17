64a8ce5a1a115ba81ac10e80338a96832a33815e
Davide Dal Pos <daveliga@gmail.com>
2024-01-17 11:58:57 -0500

    Description

---
+++ pages/Acaenitinae.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="h-full">
+      <SubfamilyAcaenitinae />
+    </div>
+  </template>
+  <script setup>
+ import SubfamilyAcaenitinae from './components/Section/Description/SubfamilyAcaenitinae.vue';
+  </script>
--- pages/components/subfamilies/subfamiliesData.js
+++ pages/components/subfamilies/subfamiliesData.js
@@ -6,7 +6,7 @@ export default [
     citations: 'OK',
     biology: 'Incomplete',
     anchor: 'Link',
-    link: '/pages/components/Section/Description/Acaenitinae'
+    link: '/Acaenitinae'
   },
   {
     id: 'Adelognathinae',
