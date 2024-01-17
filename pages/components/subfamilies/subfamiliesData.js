ca99da99668b84da2864bb23fd12b53986da7833
Davide Dal Pos <daveliga@gmail.com>
2024-01-17 10:56:59 -0500

    Table

--- pages/components/subfamilies/subfamiliesData.js
+++ pages/components/subfamilies/subfamiliesData.js
@@ -17,9 +17,368 @@ export default [
     anchor: 'Link',
     link: '/otus/659415'
   },
-
+  {
+    id: 'Agriotypinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Alomyinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Anomaloninae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Ateleutinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Banchinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Banchinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Brachycyrtinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Campopleginae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Claseinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Collyriinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Cremastinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Cryptinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Ctenopelmatinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Cylloceriinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Diacritinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Diplazontinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Eucerotinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Hybrizontinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
   {
     id: 'Ichneumoninae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Labeninae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Lycorininae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Mesochorinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Metopiinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Microleptinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Neorhacodinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Nesomesochorinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Ophioninae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Orhtocentrinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Orthopelmatinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Oxytorinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Peduncolinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Phygadeuontinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Pimplinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Poemeniinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Rhyssinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Sisyrostolinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Stilbopinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Tatogastrinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Tersilochinae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Tryphoninae',
+    numberSpecies: 'Afr',
+    distribution: 'OK',
+    citations: 'OK',
+    biology: 'Incomplete',
+    anchor: 'Link',
+    link: '/otus/659408'
+  },
+  {
+    id: 'Xoridinae',
     numberSpecies: 'Afr',
     distribution: 'OK',
     citations: 'OK',
