de7cb25b66804ea4958e05f593997e165dbb30d8
Davide Dal Pos <daveliga@gmail.com>
2024-01-18 10:33:48 -0500

    Acaenitinae

--- pages/components/Section/Description/SubfamilyAcaenitinae.vue
+++ pages/components/Section/Description/SubfamilyAcaenitinae.vue
@@ -19,9 +19,10 @@
             <VCardContent>
               <h3 class="mt-2 mb-0">Recongnition</h3>
               <p class="justify">
-                Acaenitinae females are easily recognised by the large, 
-                elongate hypopygium, although this is not so pronounced in
-                 Leptacoenites and Procinetus Förster. Other useful characters to 
+                 Acaenitinae females are easily recognised by the large, 
+                 elongate <a href="http://purl.obolibrary.org/obo/HAO_0000410">hypopygium</a>, 
+                 although this is not so pronounced in
+                 <i>Leptacoenites</i> and <i>Procinetus</i> Förster. Other useful characters to 
                  help with recognition (including of males) are: the clypeus with a 
                  median or subapical declivity, or if flat then the labrum widely exposed; 
                  the clypeus often with a median tooth; the hind wing with the nervellus 
@@ -54,6 +55,69 @@
                 The constituent genera are very narrowly defined, resulting in a relatively large number of species-poor 
                 genera and a trickle of newly described genera for species that do not fit current generic definitions 
                 (e.g. Sheng & Sun, 2010a); several undescribed taxa in BMNH do not fit within any genera as currently defined.
+              </p>
+            </VCardContent>
+          </VCard>
+          <VCard>
+            <VCardContent>
+              <h3 class="mt-2 mb-0">Biology</h3>
+              <p>
+                The few reliable rearing records for Acaenitinae are from Coleoptera. 
+                Records from xylophagous Hymenoptera and Lepidoptera have been repeated 
+                frequently in the literature but there seem to be no carefully isolated 
+                host records to support these assertions. Aubert (1969) lists various cerambycids 
+                as hosts for several species of acaenitine but his primary sources provide little information. 
+                Wahl (1990) records two separate rearings of the North American Arotes amoenus Cresson from a 
+                species of Melandryidae. Some other records are consistent, for example those of Phaenolobus 
+                species reared from cerambycid larvae in plant stems, with several records (including European specimens in BMNH) 
+                from Phytoecia and Oberea species (e.g. Fiori, 1947; Haeselbarth, 1983; Scaramozzino, 1982; Wahl, 1986). 
+                There are, intriguingly, several records of Phaenolobus species being reared from Sesiidae (Lepidoptera), 
+                the most credible of which is that of the non-British P. saltans (Gravenhorst) being reared from a sesiid 
+                mining Euphorbia stems in Iran (Karimpour et al., 2007), but parasitoids seem to have been reared in all 
+                instances from infested roots rather than from isolated hosts and several species of Cerambycidae are known 
+                to infest Euphorbia roots. Records of Procinetus decimator (Gravenhorst) being reared from the noctuid moth 
+                Gortyna flavago (Denis & Schiffermüller) relate to misidentifications of Lissonota digestor (Thunberg) (Banchinae). 
+                Previously published statements suggested variously that acaenitines are ectoparasitoids or endoparasitoids, 
+                although Wahl (1986) surmised that they were endoparasitoid, based on the larval head capsule.
+              </p>
+              <p>
+                It has been confirmed that one British species, Acaenitus dubitator (Panzer), is a koinobiont endoparasitoid, 
+                suggesting a similar mode of development for the whole subfamily (Shaw & Wahl, 1989). This has also been 
+                demonstrated for Leptacoenites notabilis (Desvignes) by Schwarz (2002a), who published a brief biological 
+                summary stating that oviposition was into early instar larvae of the weevil Otiorhynchus coecus Germar 
+                (as O. niger (Fabricius)) in their earthen cells, with the parasitoid developing as a koinobiont endoparasitoid 
+                and pupating in the hosts’ earthen cells. Schwarz (2002a) studied L. notabilis in the Austrian Alps; although 
+                it was described from a purportedly British specimen, there have been no further British specimens of L. notabilis 
+                and the Austrian host species has not been reliably recorded in Britain (Duff, 2012). The life history of 
+                A. dubitator in Scotland, including details of the egg and first, second and final larval instars, is described 
+                by Shaw & Wahl (1989). Oviposition, in early summer, is into the first and second larval instars of the weevil 
+                Cleonis pigra (Scopoli) (Coleoptera: Curculionidae) galling in the root of Cirsium arvense in sand dunes. 
+                Eggs and larvae were found free in the haemocoel of the hosts. The egg (dissected from the host) is elongate and 
+                tapered at one end. There is evidence that host development is retarded, the full grown Acaenitus larva erupting 
+                from the host larva during late July or August, when most unparasitized hosts had already pupated. The cocoon is 
+                spun within the host’s pupation chamber in the thistle root. The cocoon is similar to those of several other 
+                species which are known: cylindrical, rounded at the ends, smooth and paper-like in texture and brownish in colour. 
+                Acaenitus dubitator can overwinter in its cocoon in one of two ways. Some individuals remain as mature larvae and 
+                do not become adult the following summer, while others overwinter as morphologically distinct prepupae and are 
+                committed to pupate early in the next year and become adult. In those individuals which will become adult in the 
+                following year, the change to the prepupal condition takes place soon after cocoon formation. The mature larvae 
+                which do not become prepupae are committed to at least a one year delay in their development and do not change to 
+                prepupae until late in the following (or perhaps a further subsequent) summer. The habit of overwintering more 
+                than once as a cocooned stage is very unusual in temperate ichneumonids and in this species appears to be an 
+                adaptation to life in a particularly harsh and uncertain sand dune environment. 
+              </p>
+            </VCardContent>
+          </VCard>
+          <VCard>
+            <VCardContent>
+              <h3 class="mt-2 mb-0">References</h3>
+              <p>
+                Bennett, A.M.R. (2019) Phylogeny of the subfamilies of Ichneumonidae (Hymenoptera). 
+                <i>Journal of Hymenoptera Research</i>, 71, 1–156. 
+                <a href="https://doi.org/10.3897/jhr.71.32375">https://doi.org/10.3897/jhr.71.32375</a>
+              </p>
+              <p>
+              
               </p>
             </VCardContent>
           </VCard>
