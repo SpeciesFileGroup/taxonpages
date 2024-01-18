0c12943954540721cc586d570bb4ea392a8eb67f
Davide Dal Pos <daveliga@gmail.com>
2024-01-18 09:44:10 -0500

    Description

--- pages/components/Section/Description/SubfamilyAcaenitinae.vue
+++ pages/components/Section/Description/SubfamilyAcaenitinae.vue
@@ -1,24 +1,33 @@
 <template>
     <section>
       <div class="mx-auto py-6 prose !container dark:prose-invert">
-        <h2 class="px-4 sm:px-0">Acaenitinae</h2>
+        <h2 class="px-4 sm:px-0 justify-center">Acaenitinae</h2>
+        <span class="text-base-soft justify-center">Broad, G. & Shaw, M.</span>
         <div class="flex gap-4 flex-col">
           
           <VCard>
             <VCardContent>
-              <h3 class="mt-2 mb-0">General</h3>
-              <span class="text-base-soft">Broad, G. & Shaw, M.</span>
               <p>
-                General Information
+                This relatively small subfamily comprises about 290 species in 30, 
+                mainly small, genera (updated from Yu et al., 2012) found in the 
+                Holarctic and the Old World tropics, with one genus (Arotes) 
+                reaching South America (Castillo et al., 2011).
               </p>
             </VCardContent>
           </VCard>
           <VCard>
             <VCardContent>
-              <h3 class="mt-2 mb-0">Biology</h3>
-              <span class="text-base-soft">Broad, G. & Shaw, M.</span>
+              <h3 class="mt-2 mb-0">Recongnition</h3>
               <p>
-                Biology Information
+                Acaenitinae females are easily recognised by the large, 
+                elongate hypopygium, although this is not so pronounced in
+                 Leptacoenites and Procinetus Förster. Other useful characters to 
+                 help with recognition (including of males) are: the clypeus with a 
+                 median or subapical declivity, or if flat then the labrum widely exposed; 
+                 the clypeus often with a median tooth; the hind wing with the nervellus 
+                 intercepted above the centre; the propodeum rather flat, without distinct dorsal and 
+                 posterior slopes; the fore and mid claws sometimes with an accessory tooth; and the long ovipositor, 
+                 lacking a notch but with weak teeth on the lower valves. 
               </p>
             </VCardContent>
           </VCard>
@@ -26,4 +35,4 @@
         </div>
       </div>
     </section>
-  </template>
+  </template>justify-center
