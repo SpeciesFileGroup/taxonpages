d8e575cdaa597799c366268419bc57db837c2ee9
jlpereira <pepelui@outlook.com>
2024-05-27 20:22:52 -0300

    Add map popup for DwC data

---
+++ src/constants/objectTypes.js
@@ -0,0 +1,5 @@
+export const TYPE_MATERIAL = 'TypeMaterial'
+export const COLLECTION_OBJECT = 'CollectionObject'
+export const ASSERTED_DISTRIBUTION = 'AssertedDistribution'
+export const GEOREFERENCE = 'Georeference'
+export const AGGREGATE = 'Aggregate'
---
+++ src/modules/otus/components/Panel/PanelMap/components/MapPopup.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="max-h-32 overflow-y-auto text-xs min-w-80">
+    <ul>
+      <li
+        v-for="item in items"
+        class="py-2 last:border-0 truncate border-b"
+        title="label"
+      >
+        <span
+          v-if="item.type === COLLECTION_OBJECT"
+          class="cursor-pointer text-secondary-color"
+          v-text="item.label"
+          @click="() => emit('selected', item)"
+        />
+        <span v-else>{{ item.label }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { COLLECTION_OBJECT } from '@/constants/objectTypes.js'
+
+defineProps({
+  items: {
+    type: Array,
+    required: true
+  }
+})
+
+const emit = defineEmits(['selected'])
+</script>
