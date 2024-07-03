e4397f7c49575b7ac72c01ca91b18e50e0a430ce
jlpereira <pepelui@outlook.com>
2024-07-03 13:38:58 -0300

    Add list style for panel content. Fix pagination buttons

--- src/modules/otus/components/Panel/PanelContent/PanelContentTopic.vue
+++ src/modules/otus/components/Panel/PanelContent/PanelContentTopic.vue
@@ -2,14 +2,13 @@
   <VCardHeader class="border-t border-base-muted first:border-t-0">
     {{ title }}
   </VCardHeader>
-  <VCardContent>
-    <ul 
+  <VCardContent class="panel-content-list">
+    <div
       v-for="(text, index) in textList"
       :key="index"
-      class="pt-1 text-sm "
-    >
-      <li v-html="text" />
-    </ul>
+      class="pt-1 text-sm"
+      v-html="text"
+    ></div>
   </VCardContent>
 </template>
 
@@ -26,3 +25,18 @@ defineProps({
   }
 })
 </script>
+
+<style>
+.panel-content-list {
+  ul {
+    margin: 1rem 0;
+    list-style: disc;
+    margin-left: 1rem;
+  }
+
+  ol {
+    list-style-type: decimal;
+    margin-left: 1rem;
+  }
+}
+</style>
