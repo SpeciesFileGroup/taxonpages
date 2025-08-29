908d13b8eadbde92d73bd2bf17bf51f9a21a1229
jlpereira <pepelui@outlook.com>
2025-08-29 18:22:49 -0300

    Fix shape type

--- src/components/Map/shapes/FieldOccurrence.js
+++ src/components/Map/shapes/FieldOccurrence.js
@@ -1,4 +1,4 @@
-export const CollectionObject = {
+export const FieldOccurrence = {
   color: `rgb(var(--color-map-field-occurrence))`,
   weight: 1,
   fillOpacity: 'var(--color-map-shape-opacity)'
