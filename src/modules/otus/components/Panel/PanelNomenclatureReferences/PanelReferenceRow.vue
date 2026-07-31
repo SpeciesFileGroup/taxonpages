bd427bb9affaf61aa843af3faa4d5b581b1cc8db
Jakob Jilg <jakobjilg96@web.de>
2026-07-31 15:38:05 +0200

    made URLs clickable for DOI in nomenclature references: Using sanitizeAndLikifyHTML

--- src/modules/otus/components/Panel/PanelNomenclatureReferences/PanelReferenceRow.vue
+++ src/modules/otus/components/Panel/PanelNomenclatureReferences/PanelReferenceRow.vue
@@ -2,14 +2,14 @@
   <li class="border-b border-base-muted p-3 px-5">
     <span
       class="[&>a]:break-all block"
-      :title="stripHTML(reference)"
-      v-html="reference"
+      :title="reference"
+      v-html="sanitizeAndLinkifyHtml(reference)"
     />
   </li>
 </template>
 
 <script setup>
-import { stripHTML } from '../../../utils'
+import { sanitizeAndLinkifyHtml } from '@/utils'
 
 defineProps({
   reference: {
